2c2e2af3d16b8532f58f98a5df7c34abf1358732
Fabio Colognesi <colognesi.p.i.fabio@gmail.com>
2021-01-06 16:02:44 +0100

    Reviewed data model

--- connector_prestashop_eu_partner/models/prestashop_backend.py
+++ connector_prestashop_eu_partner/models/prestashop_backend.py
@@ -14,8 +14,12 @@ class PrestashopBackend(models.Model):
     #   comodel_name="account.fiscal.position", string="Private EU")
     account_position_business_id = fields.Many2one(
         comodel_name="account.fiscal.position", string="Business")
+    account_position_private_eu_id = fields.Many2one(
+        comodel_name="account.fiscal.position", string="Private EU")
     account_position_business_eu_id = fields.Many2one(
         comodel_name="account.fiscal.position", string="Business EU")
+    account_position_private_non_eu_id = fields.Many2one(
+        comodel_name="account.fiscal.position", string="Private not EU")
     account_position_business_non_eu_id = fields.Many2one(
         comodel_name="account.fiscal.position", string="Business not EU")
 
@@ -27,3 +31,16 @@ class PrestashopBackend(models.Model):
         comodel_name="account.account", string="National")
     account_payable_foreign_id = fields.Many2one(
         comodel_name="account.account", string="Foreign")
+
+    sale_order_type_private_id = fields.Many2one(
+        comodel_name="sale.order.type", string="Private")
+    sale_order_type_business_id = fields.Many2one(
+        comodel_name="sale.order.type", string="Business")
+    sale_order_type_private_eu_id = fields.Many2one(
+        comodel_name="sale.order.type", string="Private EU")
+    sale_order_type_business_eu_id = fields.Many2one(
+        comodel_name="sale.order.type", string="Business EU")
+    sale_order_type_private_non_eu_id = fields.Many2one(
+        comodel_name="sale.order.type", string="Private not EU")
+    sale_order_type_business_non_eu_id = fields.Many2one(
+        comodel_name="sale.order.type", string="Business not EU")
